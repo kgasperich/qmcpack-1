5b5945a8df371e73bd3f8c87176ca2a318925d35
anbenali <abenali.sci@gmail.com>
2019-05-24 14:20:21 -0500

    New HDF5 Format for QMCPACK

--- qp_convert_qmcpack_to_ezfio.py
+++ qp_convert_qmcpack_to_ezfio.py
@@ -407,6 +407,8 @@ print "END_DET"
 d_l = {'S':0, 'P':1, 'D':2, 'F':3, 'G':4, 'H':5, 'I':6}
 
 IonName=dict([('H',1),  ('He',2),  ('Li',3),('Be',4),  ('B', 5),  ('C', 6),  ('N', 7),('O', 8),  ('F', 9),   ('Ne',10),   ('Na',11),('Mg',12),   ('Al',13),   ('Si',14),   ('P', 15),   ('S', 16),('Cl',17),   ('Ar',18),   ('K', 19),   ('Ca',20),   ('Sc',21),   ('Ti',22),   ('V', 23),   ('Cr',24),   ('Mn',25),   ('Fe',26),   ('Co',27),   ('Ni',28),   ('Cu',29),   ('Zn',30),   ('Ga',31),   ('Ge',32),   ('As',33),   ('Se',34),   ('Br',35),   ('Kr',36),   ('Rb',37),   ('Sr',38),   ('Y', 39),  ('Zr',40),   ('Nb',41),   ('Mo',42),   ('Tc',43),   ('Ru',44),   ('Rh',45),   ('Pd',46),   ('Ag',47),   ('Cd',48),   ('In',49),   ('Sn',50),   ('Sb',51),   ('Te',52),   ('I', 53),   ('Xe',54),   ('Cs',55),   ('Ba',56),   ('La',57),   ('Ce',58), ('Pr',59),   ('Nd',60),   ('Pm',61),   ('Sm',62),   ('Eu',63),   ('Gd',64),   ('Tb',65),   ('Dy',66),   ('Ho',67),  ('Er',68),   ('Tm',69),   ('Yb',70),   ('Lu',71),   ('Hf',72),   ('Ta',73),   ('W', 74),   ('Re',75),   ('Os',76),   ('Ir',77),   ('Pt',78),   ('Au',79),   ('Hg',80), ('Tl',81),   ('Pb',82),  ('Bi',83),   ('Po',84),   ('At',85),   ('Rn',86),   ('Fr',87),   ('Ra',88),   ('Ac',89),   ('Th',90),   ('Pa',91),   ('U', 92),   ('Np',93)]) 
+
+
 import h5py
 from collections import defaultdict
 title="QMC"
@@ -414,9 +416,9 @@ H5_qmcpack=h5py.File(title+'.h5','w')
 
 groupApp=H5_qmcpack.create_group("application")
 CodeData  = groupApp.create_dataset("code",(1,),dtype="S15")
-CodeData[0:] = "Quantum_Package"
+CodeData[0:] = "Quantum Package"
 CodeVer  = groupApp.create_dataset("version",(3,),dtype="i4")
-CodeVer[0:] = 1
+CodeVer[0:] = 2
 CodeVer[1:] = 0
 CodeVer[2:] = 0
 
@@ -576,14 +578,11 @@ for x in range(NbSpecies):
     atomicBasisSetGroup.create_dataset("NbBasisGroups",(1,),dtype="i4",data=nshell)
 
 Complex=False
-UnRestricted=False
+Restricted=True
 GroupParameter.create_dataset("IsComplex",(1,),dtype="b1",data=Complex)
-GroupParameter.create_dataset("SpinUnResticted",(1,),dtype="b1",data=UnRestricted)
+GroupParameter.create_dataset("SpinRestricted",(1,),dtype="b1",data=Restricted)
 
-GroupNbkpts=H5_qmcpack.create_group("Nb_KPTS")
-Nbkpts=1
-GroupNbkpts.create_dataset("Nbkpts",(1,),dtype="i4",data=Nbkpts)
-GroupDet=H5_qmcpack.create_group("KPTS_0")
+GroupDet=H5_qmcpack.create_group("Super_Twist")
 NbMO=mo_num
 NbAO=ao_num
 print  "mo_num=",NbMO
